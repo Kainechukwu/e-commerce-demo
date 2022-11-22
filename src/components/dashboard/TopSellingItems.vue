0f0de45154f4487f15106f0497a5067f0a6ffb56
Kaine Bismarck <kaine.bismarck@suprbiz.io>
2022-11-22 22:12:07 +0100

    trending products carousel update

--- src/components/dashboard/TopSellingItems.vue
+++ src/components/dashboard/TopSellingItems.vue
@@ -2,7 +2,7 @@
     <div class="bg-white">
         <div class="mx-auto max-w-2xl py-6 px-4 sm:py-8 sm:px-6 lg:max-w-7xl lg:px-8">
             <div class="md:flex md:items-center md:justify-between">
-                <h2 class="text-2xl font-bold tracking-tight text-gray-900">Princess's Trending products</h2>
+                <h2 class="text-2xl font-bold tracking-tight text-gray-900">Trending products</h2>
                 <a href="#" class="hidden text-sm font-medium text-brand-600 hover:text-indigo-500 md:block">
                     Shop the collection
                     <span aria-hidden="true"> &rarr;</span>
@@ -33,28 +33,33 @@
             </div>
         </div>
 
-        <carousel v-model="slideIndex" :autoplay="5000" :wrap-around="true" :transition="2000">
-            <slide v-for="product in products" :key="product.id" class="group relative">
-                <div class="carousel__item">
-                    <div class="h-56 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:h-72 xl:h-80">
-                        <img :src="product.imageSrc" :alt="product.imageAlt" class="h-full w-full object-cover object-center" />
+        <div class="px-4 sm:px-6 lg:px-8">
+            <carousel v-model="slideIndex" :autoplay="3000" :wrap-around="true" :transition="1000" :itemsToShow="3.95">
+                <slide v-for="product in products" :key="product.id" class="group relative">
+                    <div class="carousel__item">
+                        <div
+                            :class="product.id === products[slideIndex].id ? 'lg:h-80 xl:h-[22rem]' : ''"
+                            class="h-56 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:h-72 xl:h-80"
+                        >
+                            <img :src="product.imageSrc" :alt="product.imageAlt" class="h-full w-full object-cover object-center" />
+                        </div>
+                        <h3 class="mt-4 text-sm text-gray-700">
+                            <a :href="product.href">
+                                <span class="absolute inset-0" />
+                                {{ product.name }}
+                            </a>
+                        </h3>
+                        <p class="mt-1 text-sm text-gray-500">{{ product.color }}</p>
+                        <p class="mt-1 text-sm font-medium text-gray-900">{{ product.price }}</p>
                     </div>
-                    <h3 class="mt-4 text-sm text-gray-700">
-                        <a :href="product.href">
-                            <span class="absolute inset-0" />
-                            {{ product.name }}
-                        </a>
-                    </h3>
-                    <p class="mt-1 text-sm text-gray-500">{{ product.color }}</p>
-                    <p class="mt-1 text-sm font-medium text-gray-900">{{ product.price }}</p>
-                </div>
-            </slide>
+                </slide>
 
-            <template #addons>
-                <navigation />
-                <!-- <pagination class="" /> -->
-            </template>
-        </carousel>
+                <template #addons>
+                    <navigation />
+                    <!-- <pagination class="" /> -->
+                </template>
+            </carousel>
+        </div>
 
         <div class="flex z-40 gap-4 items-center justify-center mt-12 mb-[50px]">
             <button
